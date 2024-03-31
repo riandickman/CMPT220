4b22502d2eb5e2d82c49eaa83e37ce7983fe719b
riandickman <147005520+riandickman@users.noreply.github.com>
2024-03-31 19:56:42 -0400

    Add files via upload

--- Homework 4/Main.java
+++ Homework 4/Main.java
@@ -1,51 +1,37 @@
-import java.util.Arrays;
-//Our cheaty array helper file <3
+
+/*
+Take the given array and keep track of:
+1. Any duplicate numbers
+2. How many times the duplicate numbers appear
+3. Print out those duplicate numbers with how many times they appear
+
+ */
+
 public class Main {
     public static void main(String[] args) {
+        //Don't let the size of this scare you! no matter how big an array is, it all works the same!
+        int[] myArray = {10, 3, 295, 38, 20, 3, 4, 267, 2445, 10, 5566, 87, 93, 17, 10, 2, 87, 267, 3176, 3, 82};
+        //you cannot use the array util. Do this one by hand :(
+        boolean[] duplicateprinted = new boolean[myArray.length];
+        for (int i = 0; i < myArray.length; i++) {
+            int count = 0;
+            if (!duplicateprinted[i]) {
+                for (int j = 0; j < myArray.length; j++) {
+                    if (myArray[i] == myArray[j]) {
+                        count++;
+                        duplicateprinted[j] = true;
 
-        //Create me an array named "nameArray" that is a string and has 4 values in it, give me 4 names of people or animals you care about
-        String[] nameArray = {"Kellen", "Laurie", "Tim", "Mack"};
-        //Create me an array without any values, but it's an integer array named "numArray"
-        int[] numArray = {};
-        //Create me an int array named "newArray" and set it to have 20 null spaces
-        int[] newArray = new int[20];
-        int nullvalue = -1;
-        for (int i = 0; i < newArray.length; i++) {
-            newArray[i] = nullvalue;
+                    }
+                }
+                if (count > 1) {
+                    System.out.println("Duplicate:" + myArray[i]);
+                    System.out.println("Number of occurrences " + count);
+                    duplicateprinted[i] = true;
+                }
 
-
-
-
-        //Here's our first practice array!
-        String[] sortMe = {"Apple","Kiwi","Orange","Banana","Watermelon","Strawberry"};
-        //What method is missing here to actually sort our array?
-        Arrays.sort(sortMe);
-        //And what method is missing here to print out that array?
-        System.out.println(Arrays.toString(sortMe));
-        //After this, you cannot use the array helper file, gotta go back to the old ways!!
-
-        //another one!
-        int addMe[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        //How would we add every index together and print out the sum??
-        //A small hint:
-        int sum = 0;
-
-        for (int m = 0; m < addMe.length; m++) {
-            sum += addMe[m];
-
-    }System.out.println("Sum of array: " + sum);
-
-
-        //thinking one! here's an array
-        int maxMe[] = {280, 4892, 134, 23, 1937, 5329, 932, 89};
-        int large = maxMe[0];
-        for (int j = 0; j < maxMe.length; j++) {
-            if (maxMe[j] > large) {
-                large = maxMe[j];
             }
 
-        //What would you have to do to find the largest number and then print that value?
 
-
-    }System.out.println("Largest number: " + large);
-}}}
+        }
+    }
+}
